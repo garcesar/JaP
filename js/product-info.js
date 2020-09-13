465574c2b3e63b56fee8ef25e616ec3d599a0350
cesar.garcia <garcesar.ve@gmail.com>
2020-09-13 18:36:20 -0300

    Se trabajo sobre los comentarios y su califación

--- js/product-info.js
+++ js/product-info.js
@@ -3,156 +3,162 @@ var productComment = {};
 var productRelated = {};
 
 //Funtion que muestra la galeria de productos
-function showImagesGallery(array){
+    function showImagesGallery(array){
 
-    let htmlContentToAppend = "";
+        let htmlContentToAppend = "";
 
-    for(let i = 0; i < array.length; i++){
-        let imageSrc = array[i];
+        for(let i = 0; i < array.length; i++){
+            let imageSrc = array[i];
 
-        htmlContentToAppend += `
-        <div class="col-lg-3 col-md-4 col-6">
-            <div class="d-block mb-4 h-100">
-                <img class="img-fluid img-thumbnail" src="` + imageSrc + `" alt="">
+            htmlContentToAppend += `
+            <div class="col-lg-3 col-md-4 col-6">
+                <div class="d-block mb-4 h-100">
+                    <img class="img-fluid img-thumbnail" src="` + imageSrc + `" alt="">
+                </div>
             </div>
-        </div>
-        `
+            `
 
-        document.getElementById("product-image-container").innerHTML = htmlContentToAppend;
+            document.getElementById("product-image-container").innerHTML = htmlContentToAppend;
+        }
     }
-}
-
-//Funtion que muestra los comentarios
-function showComments(array){
-
-    let htmlContentToAppend = "";
-
-    for(let i = 0; i < array.length; i++){
-        let comment = array[i];
-
-        A = `<span class= "fa fa-star checked"></span>`;
-        a = A.repeat(comment.score);
-        B = `<span class= "fa fa-star"></span>`;
-        b = B.repeat(5 - comment.score);
-
-        htmlContentToAppend += `
-
-        <div class="row">
-            <div class="col">
-                <div class="d-flex w-100 justify-content-between">
-                    <h6 class="mb-1"><strong>`+ comment.user +": " + `</strong> `+ a + b +` </h6>
-                </div>
-                <div class="d-flex w-100 justify-content-between">
-                    <p class="mb-1">`+ comment.description +`</p>
-                </div>
-                <small class="text-muted">`+ comment.dateTime +`</small>
-                </div>
-                </br>
-            </div>
-        </div>
-    
-        `        
-    }
-    document.getElementById("prod-comment-container").innerHTML = htmlContentToAppend;
-}
 
 // Funtion que muestra los productos relacionados
-function showRelated(array){
+    function showRelated(array){
 
-    let htmlContentToAppend = "";
+        let htmlContentToAppend = "";
 
-    for(let i = 0; i < array.length; i++){
-        let related = array[i];
+        for(let i = 0; i < array.length; i++){
+            let related = array[i];
 
-        if(related.imgSrc === "img/prod2.jpg"){
-            htmlContentToAppend += `
-            <div class="col-lg-3 col-md-4 col-6">
-                <div class="d-block mb-4 h-100">
-                    <img class="img-fluid img-thumbnail" src="` + related.imgSrc + `" alt="">
+            if(related.imgSrc === "img/prod2.jpg"){
+                htmlContentToAppend += `
+                <div class="col-lg-3 col-md-4 col-6">
+                    <div class="d-block mb-4 h-100">
+                        <img class="img-fluid img-thumbnail" src="` + related.imgSrc + `" alt="">
+                    </div>
                 </div>
-            </div>
-            `
-        }     
-    }
+                `
+            }     
+        }
 
-    for(let i = 0; i < array.length; i++){
-        let related = array[i];
+        for(let i = 0; i < array.length; i++){
+            let related = array[i];
 
-        if(related.imgSrc === "img/prod4.jpg"){
-            htmlContentToAppend += `
-            <div class="col-lg-3 col-md-4 col-6">
-                <div class="d-block mb-4 h-100">
-                    <img class="img-fluid img-thumbnail" src="` + related.imgSrc + `" alt="">
+            if(related.imgSrc === "img/prod4.jpg"){
+                htmlContentToAppend += `
+                <div class="col-lg-3 col-md-4 col-6">
+                    <div class="d-block mb-4 h-100">
+                        <img class="img-fluid img-thumbnail" src="` + related.imgSrc + `" alt="">
+                    </div>
                 </div>
-            </div>
-            `
-        }     
+                `
+            }     
+        }
+        document.getElementById("product-related-container").innerHTML = htmlContentToAppend;
     }
-    document.getElementById("product-related-container").innerHTML = htmlContentToAppend;
-}
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
-        if (resultObj.status === "ok")
-        {
-            product = resultObj.data;
+    document.addEventListener("DOMContentLoaded", function(e){
+        getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
+            if (resultObj.status === "ok")
+            {
+                product = resultObj.data;
 
-            let productNameHTML  = document.getElementById("productName");
-            let productCostNameHTML = document.getElementById("productCost")
-            let productDescriptionHTML = document.getElementById("productDescription");
-            let productCategoryHTML = document.getElementById("productCategory");
-            let productCountHTML = document.getElementById("productSoldCount");
+                let productNameHTML  = document.getElementById("productName");
+                let productCostNameHTML = document.getElementById("productCost")
+                let productDescriptionHTML = document.getElementById("productDescription");
+                let productCategoryHTML = document.getElementById("productCategory");
+                let productCountHTML = document.getElementById("productSoldCount");
+            
+                productNameHTML.innerHTML = product.name;
+                productCostNameHTML.innerHTML = product.cost + " " + product.currency;
+                productDescriptionHTML.innerHTML = product.description;
+                productCategoryHTML.innerHTML = product.category;
+                productCountHTML.innerHTML = product.soldCount;
+
+//Muestro las imagenes en forma de galería
+                showImagesGallery(product.images);
+            }
+        });
         
-            productNameHTML.innerHTML = product.name;
-            productCostNameHTML.innerHTML = product.cost + " " + product.currency;
-            productDescriptionHTML.innerHTML = product.description;
-            productCategoryHTML.innerHTML = product.category;
-            productCountHTML.innerHTML = product.soldCount;
-
-            //Muestro las imagenes en forma de galería
-            showImagesGallery(product.images);
-        }
     });
-    
-});
 
-document.addEventListener("DOMContentLoaded", function(e){
+//Productos
+    document.addEventListener("DOMContentLoaded", function(e){
+        getJSONData(PRODUCTS_URL).then(function(resultObj){
+            if (resultObj.status === "ok")
+            {
+                productRelated = resultObj.data;
+// Function que muestra la relación de los productos
+                showRelated(productRelated);
+            }
+        });    
+    });
+
+//Comentarios:
     getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
         if (resultObj.status === "ok")
         {
             productComment = resultObj.data;
-            // Function que muestra los comentarios
+
             showComments(productComment);
         }
-    });    
-});
+    }); 
 
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCTS_URL).then(function(resultObj){
-        if (resultObj.status === "ok")
-        {
-            productRelated = resultObj.data;
-            // Function que muestra la relación de los productos
-            showRelated(productRelated);
+//Funtion que lista los comentarios
+    function showComments(array){
+
+        let htmlContentToAppend = "";
+
+        for(let i = 0; i < array.length; i++){
+            let comment = array[i];
+
+            A = `<span class= "fa fa-star checked"></span>`;
+            a = A.repeat(comment.score);
+            B = `<span class= "fa fa-star"></span>`;
+            b = B.repeat(5 - comment.score);
+
+            htmlContentToAppend += `
+                <div class="list-group-item list-group-item-action">
+                    <div class="row">
+                        <div class="col-4">
+                            <h6 class="mb-1"><strong>`+ " " + comment.user +": " + `</strong> `+ a + b +` </h6>
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <p class="mb-10">`+ comment.description +`</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <small class="text-muted">`+ comment.dateTime +`</small>        
+                    </div>
+                </div>
+                `       
         }
-    });    
-});
+        document.getElementById("prod-comment-container").innerHTML = htmlContentToAppend;
+    }
+// Funtion que guarda los datos del formulario para un nuevo comentario
+// Se almacena en sessionStorage
+// Se realiza un push cobre el array de los comentarios del JSON
+// Se llama a la funtion que lista los comentarios
 
-//Función que envía los comenarios
+function up_comment(){    
 
-var commentAdd = [];
+        let new_comment = document.getElementById("new_comment").value;
+        sessionStorage.setItem("new_coment", new_comment);
 
-document.getElementById("my-form").addEventListener("click", function(){
+        let score = document.getElementById("score").value;
+        sessionStorage.setItem("score", score);
 
-    let commentContainer = document.getElementById("new-comment").value;
-    let scoreContainer = document.getElementById("score").value;
+        productComment.push ({
+            "description": document.getElementById("new_comment").value,
+            "user": sessionStorage.getItem("name"),
+            "score": sessionStorage.getItem("score"),
+            "dateTime": new Date()
+        });
 
-    let commentTotal =  ` <div> ` + commentContainer + ` <br> </div> <div> ` + scoreContainer + ` </div> `;
-   
-    commentAdd.push(commentTotal);
-    document.getElementById("prod-comment-container").innerHTML += commentAdd;
-
-}); 
+        showComments(productComment);
+    }
--- js/products.js
+++ js/products.js
@@ -6,153 +6,154 @@ var currentSortCriteria = undefined;
 var minCount = undefined;
 var maxCount = undefined;
 
-function sortCategories(criteria, array){
-    let result = [];
-    if (criteria === ORDER_ASC)
-    {
-        result = array.sort(function(a1, b2) {
-            let a = parseInt(a1.cost);
-            let b = parseInt(b2.cost);
-            
-            if ( a < b ){ return -1; }
-            if ( a > b ){ return 1; }
-            return 0;
-        });
-    }else if (criteria === ORDER_DESC){
-        result = array.sort(function(a1, b2) {
-            let a = parseInt(a1.cost);
-            let b = parseInt(b2.cost);
-
-            if ( a > b ){ return -1; }
-            if ( a < b ){ return 1; }
-            return 0;
-        });
-    }else if (criteria === ORDER_BY_PROD_COUNT){
-        result = array.sort(function(a, b) {
-            let aCount = parseInt(a.soldCount);
-            let bCount = parseInt(b.soldCount);
-
-            if ( aCount > bCount ){ return -1; }
-            if ( aCount < bCount ){ return 1; }
-            return 0;
-        });
-    }
-
-    return result;
-}
-
-function showProductsList(){
-
-    let htmlContentToAppend = "";
-    for(let i = 0; i < currentProductArray.length; i++){
-        let product = currentProductArray[i];
-
-        if (((minCount == undefined) || (minCount != undefined && parseInt(product.soldCount) >= minCount)) &&
-            ((maxCount == undefined) || (maxCount != undefined && parseInt(product.soldCount) <= maxCount))){
-
-            htmlContentToAppend += `
-            <a href="product-info.html" class="list-group-item list-group-item-action">
-                <div class="row">
-                    <div class="col-3">
-                        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
-                    </div>
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">`+ product.name +`</h4>
-                            <small class="text-muted">` + product.soldCount + ` artículos</small>
-                        </div>
-                        <p class="mb-1">` + product.description + `</p>
-                        <p class ="mb-1"> `+ product.cost + ' ' + product.currency + ` </p>
-                    </div>
-                </div>
-            </a>
-            `
-        }
-
-        document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
-    }
-}
-
-showSpinner();
-document.addEventListener("DOMContentLoaded", function (e) {
-    getJSONData(PRODUCTS_URL).then(function(resultObj){
-        if (resultObj.status === "ok")
+// Comienzo de filtrado
+    function sortCategories(criteria, array){
+        let result = [];
+        if (criteria === ORDER_ASC)
         {
-            currentProductArray = resultObj.data;
-            //Muestro las categorías ordenadas
-            showProductsList(currentProductArray);
+            result = array.sort(function(a1, b2) {
+                let a = parseInt(a1.cost);
+                let b = parseInt(b2.cost);
+                
+                if ( a < b ){ return -1; }
+                if ( a > b ){ return 1; }
+                return 0;
+            });
+        }else if (criteria === ORDER_DESC){
+            result = array.sort(function(a1, b2) {
+                let a = parseInt(a1.cost);
+                let b = parseInt(b2.cost);
+
+                if ( a > b ){ return -1; }
+                if ( a < b ){ return 1; }
+                return 0;
+            });
+        }else if (criteria === ORDER_BY_PROD_COUNT){
+            result = array.sort(function(a, b) {
+                let aCount = parseInt(a.soldCount);
+                let bCount = parseInt(b.soldCount);
+
+                if ( aCount > bCount ){ return -1; }
+                if ( aCount < bCount ){ return 1; }
+                return 0;
+            });
         }
-        hideSpinner();
+
+        return result;
+    }
+
+    function showProductsList(){
+
+        let htmlContentToAppend = "";
+        for(let i = 0; i < currentProductArray.length; i++){
+            let product = currentProductArray[i];
+
+            if (((minCount == undefined) || (minCount != undefined && parseInt(product.soldCount) >= minCount)) &&
+                ((maxCount == undefined) || (maxCount != undefined && parseInt(product.soldCount) <= maxCount))){
+
+                htmlContentToAppend += `
+                <a href="product-info.html" class="list-group-item list-group-item-action">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h4 class="mb-1">`+ product.name +`</h4>
+                                <small class="text-muted">` + product.soldCount + ` artículos</small>
+                            </div>
+                            <p class="mb-1">` + product.description + `</p>
+                            <p class ="mb-1"> `+ product.cost + ' ' + product.currency + ` </p>
+                        </div>
+                    </div>
+                </a>
+                `
+            }
+
+            document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
+        }
+    }
+// Spinner
+    showSpinner();
+    document.addEventListener("DOMContentLoaded", function (e) {
+        getJSONData(PRODUCTS_URL).then(function(resultObj){
+            if (resultObj.status === "ok")
+            {
+                currentProductArray = resultObj.data;
+                //Muestro las categorías ordenadas
+                showProductsList(currentProductArray);
+            }
+            hideSpinner();
+        });
     });
-});
 
 // Continua el filtrado
 
-function sortAndShowCategories(sortCriteria, categoriesArray){
-    currentSortCriteria = sortCriteria;
+    function sortAndShowCategories(sortCriteria, categoriesArray){
+        currentSortCriteria = sortCriteria;
 
-    if(categoriesArray != undefined){
-        currentProductArray = categoriesArray;
+        if(categoriesArray != undefined){
+            currentProductArray = categoriesArray;
+        }
+
+        currentProductArray = sortCategories(currentSortCriteria, currentProductArray);
+
+        //Muestro las categorías ordenadas
+        showProductsList();
     }
-
-    currentProductArray = sortCategories(currentSortCriteria, currentProductArray);
-
-    //Muestro las categorías ordenadas
-    showProductsList();
-}
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCTS_URL).then(function(resultObj){
-        if (resultObj.status === "ok"){
-            sortAndShowCategories(ORDER_ASC, resultObj.data);
-        }
-    });
+    document.addEventListener("DOMContentLoaded", function(e){
+        getJSONData(PRODUCTS_URL).then(function(resultObj){
+            if (resultObj.status === "ok"){
+                sortAndShowCategories(ORDER_ASC, resultObj.data);
+            }
+        });
 
-    document.getElementById("sortAsc").addEventListener("click", function(){
-        sortAndShowCategories(ORDER_ASC);
-    });
+        document.getElementById("sortAsc").addEventListener("click", function(){
+            sortAndShowCategories(ORDER_ASC);
+        });
 
-    document.getElementById("sortDesc").addEventListener("click", function(){
-        sortAndShowCategories(ORDER_DESC);
-    });
+        document.getElementById("sortDesc").addEventListener("click", function(){
+            sortAndShowCategories(ORDER_DESC);
+        });
 
-    document.getElementById("sortByCount").addEventListener("click", function(){
-        sortAndShowCategories(ORDER_BY_PROD_COUNT);
-    });
+        document.getElementById("sortByCount").addEventListener("click", function(){
+            sortAndShowCategories(ORDER_BY_PROD_COUNT);
+        });
 
-    document.getElementById("clearRangeFilter").addEventListener("click", function(){
-        document.getElementById("rangeFilterCountMin").value = "";
-        document.getElementById("rangeFilterCountMax").value = "";
+        document.getElementById("clearRangeFilter").addEventListener("click", function(){
+            document.getElementById("rangeFilterCountMin").value = "";
+            document.getElementById("rangeFilterCountMax").value = "";
 
-        minCount = undefined;
-        maxCount = undefined;
-
-        showProductsList();
-    });
-
-    document.getElementById("rangeFilterCount").addEventListener("click", function(){
-        //Obtengo el mínimo y máximo de los intervalos para filtrar por cantidad
-        //de productos por categoría.
-        minCount = document.getElementById("rangeFilterCountMin").value;
-        maxCount = document.getElementById("rangeFilterCountMax").value;
-
-        if ((minCount != undefined) && (minCount != "") && (parseInt(minCount)) >= 0){
-            minCount = parseInt(minCount);
-        }
-        else{
             minCount = undefined;
-        }
-
-        if ((maxCount != undefined) && (maxCount != "") && (parseInt(maxCount)) >= 0){
-            maxCount = parseInt(maxCount);
-        }
-        else{
             maxCount = undefined;
-        }
 
-        showProductsList();
+            showProductsList();
+        });
+
+        document.getElementById("rangeFilterCount").addEventListener("click", function(){
+            //Obtengo el mínimo y máximo de los intervalos para filtrar por cantidad
+            //de productos por categoría.
+            minCount = document.getElementById("rangeFilterCountMin").value;
+            maxCount = document.getElementById("rangeFilterCountMax").value;
+
+            if ((minCount != undefined) && (minCount != "") && (parseInt(minCount)) >= 0){
+                minCount = parseInt(minCount);
+            }
+            else{
+                minCount = undefined;
+            }
+
+            if ((maxCount != undefined) && (maxCount != "") && (parseInt(maxCount)) >= 0){
+                maxCount = parseInt(maxCount);
+            }
+            else{
+                maxCount = undefined;
+            }
+
+            showProductsList();
+        });
     });
-});
