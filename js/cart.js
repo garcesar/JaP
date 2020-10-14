d77e8884109b0f3f5dd24d867055efcbe7e6df67
cesar.garcia <garcesar.ve@gmail.com>
2020-10-14 07:54:05 -0300

    Se trabajo en porcentaje de envio en el carrito de compras

--- js/cart.js
+++ js/cart.js
@@ -7,31 +7,39 @@ var num = 0;
 var num2 = 0;
 var total1;
 var total2;
+let comissionPercentage = 0.13;
 
-//Función que actualiza el costo final
-function showTotal(){
-    let showT = document.getElementById("total");
+//Función que actualiza los costos de sub total, envio y total
+function showCost(){
     let showP = document.getElementById("productCostText");
+    let showE = document.getElementById("envioCost");
+    let showT = document.getElementById("totalCost");
+    let subtotal;
     let total;
+    let envio;
     let conv = dolar * 12500;
 
     num = localStorage.getItem("number1");
     num2 = localStorage.getItem("number2");
     
-    total = (num * 100) + (conv * num2);
+    subtotal = (num * 100) + (conv * num2);
+    envio = Math.round((comissionPercentage * subtotal));
+    total = subtotal + envio;
+
+    showP.innerHTML = 'UYU' + ' ' + subtotal;
+    showE.innerHTML = 'UYU' + ' ' + envio;
     showT.innerHTML = 'UYU' + ' ' + total;
-    showP.innerHTML = 'UYU' + ' ' + total;
 }
 
 document.addEventListener("DOMContentLoaded", function (ex) {
-
+    
 //Función que obtiene el numero ingresado en el input, lo guarda en una variable y se imprime en sub Total
     document.getElementById("number1").addEventListener("click", function () {
         num = document.getElementById("number1").value;
         localStorage.setItem("number1", num);
         total1 = num * 100;
         document.getElementById("subTotal").innerHTML ='UYU'  + ' ' + total1;
-        showTotal()
+        showCost()
     });
 
 //Función que obtiene el numero ingresado en el input, lo guarda en una variable y se imprime en sub Total
@@ -40,9 +48,25 @@ document.addEventListener("DOMContentLoaded", function (ex) {
         localStorage.setItem("number2", num2);
         total2 = (40 * 12500) * num2;
         document.getElementById("subTotal2").innerHTML ='UYU'  + ' ' + total2;
-        showTotal()
+        showCost()
     });
 
+//Función que calcula el costo de envio
+    document.getElementById("premiumradio").addEventListener("change", function(){
+        comissionPercentage = 0.15;
+        showCost();
+    });
+
+    document.getElementById("expressradio").addEventListener("change", function(){
+        comissionPercentage = 0.07;
+        showCost();
+    });
+
+    document.getElementById("standardradio").addEventListener("change", function(){
+        comissionPercentage = 0.05;
+        showCost();
+    });
+    
     getJSONData(CART_TWO_INFO_URL).then(function (resultObj) {
         if (resultObj.status === "ok") {
             art = resultObj.data;
